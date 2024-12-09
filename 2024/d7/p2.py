1c0f67bd22ad68d3efa111641d4a9a73dac589b0
dscutti <dascutti@gmail.com>
2024-12-09 13:38:34 -0600

    DaleS - day7 p2

--- 2024/d7/p2.py
+++ 2024/d7/p2.py
@@ -8,6 +8,8 @@ class Num:
         return Num(self.i + other.i)
     def __sub__(self, other):
         return Num(self.i * other.i)
+    def __mul__(self, other):
+        return str(self.i)+str()
 
 def put_at_idx(base, idx, char):
     return base[:idx]+char+base[idx+1:]
@@ -28,28 +30,41 @@ def my_eval(x):
     return eval(s).i
 
 if __name__ == '__main__':
+    bitness = 3
+    for i in range(3**bitness):
+        print(f'i = {i}: {(i//3**3)%3}-{(i//3**2)%3}-{(i//3**1)%3}-{i%3}')
+        op = ''.join([f'{(i//3**j)%3}' for j in range(bitness-1, -1, -1)])
+
+        print(op)
+
 
     f = open('in2.txt')
     inp = f.read().split('\n')
 
     sum = 0
+    num_good = 0
     for line in inp:
         ans, expr = line.split(':')
         ans = int(ans)
         expr = expr.strip()
-        spaces = [i for i, x in enumerate(expr) if x == ' ']
-        for x in range(2**len(spaces)):
-            bin_str = ('{0:0' + str(len(spaces)) + 'b}').format(x)
-            for i in range(len(bin_str)):
-                if bin_str[i] == '0':
-                    expr = put_at_idx(expr, spaces[i], '+')
-                else:
-                    # subtract is multiply
-                    expr = put_at_idx(expr, spaces[i], '-')
-            result = my_eval(expr)
-            if result == ans:
+        nums = [int(x) for x in expr.split(' ')]
+        bits = len(nums)-1
+        for x in range(3**bits):
+            op_str = ''.join([f'{(x//3**j)%3}' for j in range(bits-1, -1, -1)])
+            acc = nums[0]
+            for i in range(len(op_str)):
+                match op_str[i]:
+                    case '0':
+                        acc += nums[i+1]
+                    case '1':
+                        acc *= nums[i+1]
+                    case '2':
+                        acc = int(str(acc)+str(nums[i+1]))
+            if acc == ans:
                 #expr = expr.replace('-', '+')
-                sum += ans
+                sum += acc
+                num_good += 1
                 break
     print(sum)
+    print(num_good)
 

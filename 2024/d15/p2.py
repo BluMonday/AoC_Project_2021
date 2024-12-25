080711d4953e76fceaf4de650d19fee541798bfb
dscutti <dascutti@gmail.com>
2024-12-25 14:10:20 -0600

    DaleS - day15 p2, close

--- 2024/d15/p2.py
+++ 2024/d15/p2.py
@@ -31,23 +31,92 @@ class Point(namedtuple('Point', 'x y')):
 
     def gps_coord(self):
         return 100*self.y + self.x
+def try_move_boxes(start_row, mov_dir):
+    # start with list of '['
+    next_pts = []
+    for pt in start_row:
+        # adjacent to each [
+        next_pts.append(mov_dir(pt))
+        # adjacent to each ]
+        next_pts.append(mov_dir(pt.right()))
+    next_things = [grid[x] for x in next_pts]
 
-def try_move(start_pt, mov_dir):
+    if all([x in '.' for x in next_things]):
+        # just move
+        for pt in start_row:
+            grid[pt] = '.'
+            grid[pt.right()] = '.'
+        for pt in start_row:
+            grid[mov_dir(pt)] = '['
+            grid[mov_dir(pt.right())] = ']'
+        return True
+    elif any([x in '#' for x in next_things]):
+        # can't move
+        return False
+    else :
+        # touching only boxes and empty space
+        # collect left edges of next boxes and call recursively
+        next_row = [p for p, t in zip(next_pts, next_things) if t in '[']
+        for p, t in zip(next_pts, next_things):
+            if t in ']': next_row.append(p.left())
+        next_boxes_movable = try_move_boxes(next_row, mov_dir)
+        if next_boxes_movable:
+            # move this thing
+            for pt in start_row:
+                grid[pt] = '.'
+                grid[pt.right()] = '.'
+            for pt in start_row:
+                grid[mov_dir(pt)] = '['
+                grid[mov_dir(pt.right())] = ']'
+            return True
+        else:
+            # don't move
+            return False
+
+def try_move_h(start_pt, mov_dir):
     thing = grid[start_pt]
     next_pt = mov_dir(start_pt)
     next_thing = grid[next_pt]
 
-    if next_thing == '.':
+    if next_thing in '.':
         # just move
         grid[start_pt] = '.'
         grid[next_pt] = thing
         return True
-    elif next_thing == '#':
+    elif next_thing in '#':
         # can't move
         return False
-    elif next_thing == 'O':
+    elif next_thing in '[]':
         # check if next object can move
-        next_thing_movable = try_move(next_pt, mov_dir)
+        next_thing_movable = try_move_h(next_pt, mov_dir)
+        if next_thing_movable:
+            # move this thing
+            grid[start_pt] = '.'
+            grid[next_pt] = thing
+            return True
+        else:
+            # don't move
+            return False
+
+def try_move_v(start_pt, mov_dir):
+    thing = grid[start_pt]
+    next_pt = mov_dir(start_pt)
+    next_thing = grid[next_pt]
+
+    if next_thing in '.':
+        # just move
+        grid[start_pt] = '.'
+        grid[next_pt] = thing
+        return True
+    elif next_thing in '#':
+        # can't move
+        return False
+    elif next_thing in '[]':
+        # check if next object can move
+        next_row = next_pt
+        if next_thing in ']':
+            next_row = next_pt.left()
+        next_thing_movable = try_move_boxes([next_row], mov_dir)
         if next_thing_movable:
             # move this thing
             grid[start_pt] = '.'
@@ -67,32 +136,42 @@ class Gaurd:
         global height
         global width
         mov_dir = Point.right
-        rev_dir = Point.left
         match cmd:
             case '<':
                 mov_dir = Point.left
-                rev_dir = Point.right
             case '>':
                 mov_dir = Point.right
-                rev_dir = Point.left
             case '^':
                 mov_dir = Point.up
-                rev_dir = Point.down
             case 'v':
                 mov_dir = Point.down
-                rev_dir = Point.up
-
-        if try_move(self.position, mov_dir):
-            self.position = mov_dir(self.position)
+        if mov_dir in [Point.left, Point.right]:
+            if try_move_h(self.position, mov_dir):
+                self.position = mov_dir(self.position)
+        else:
+            if try_move_v(self.position, mov_dir):
+                self.position = mov_dir(self.position)
         return
 
 def find_in_grid(value):
     for k, v in grid.items():
         if v == value:
             return k
+def convert_for_p2(input):
+    match input:
+        case '.':
+            return '..'
+        case 'O':
+            return '[]'
+        case '@':
+            return '@.'
+        case '#':
+            return '##'
+
 def init_grid(inp):
     for y, line in enumerate(inp):
-        for x, c in enumerate(line):
+        p2_line = ''.join([convert_for_p2(x) for x in line])
+        for x, c in enumerate(p2_line):
             pt = Point(x,y)
             grid[pt] = c
 
@@ -114,7 +193,7 @@ def print_grid(move, move_dir):
 
 if __name__ == '__main__':
 
-    config = 2
+    config = 3
     f = open(f'in{config}.txt')
     inp = f.read().split('\n\n')
     grid_inp = inp[0].split('\n')
@@ -126,7 +205,7 @@ if __name__ == '__main__':
     guard = Gaurd()
     guard.position = find_in_grid('@')
 
-    width = len(grid_inp[0])
+    width = len(grid_inp[0])*2
     height = len(grid_inp)
 
 
